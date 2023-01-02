1c8668e3d89fafaecee66b72385695c56aede75b
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-01-02 22:33:45 +0600

    update data

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-
+import Loading from "../Shared/Loading";
+import UserRow from "./UserRow";
 const Users = () => {
-  const { data } = useQuery("users", () => fetch("").then((res) => res.json()));
+  const { data: users, isLoading } = useQuery("users", () =>
+    fetch("http://localhost:5000/user").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h1>All users</h1>
+      <h1>All users :{users.length} </h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <UserRow key={user._id} user={user}></UserRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
